7e7fe70ac09756987f83d698cd4fffa135471534
Sreetam Das <sreetamdas@gmail.com>
2022-12-02 14:04:03 +0530

    feat: day 02

---
+++ lib/2022/day_02/day_02.ex
@@ -0,0 +1,69 @@
+defmodule RockPaperScissors do
+  @moduledoc """
+  Solution for Day 2 of Advent of Code 2022: Rock Paper Scissors
+  """
+  alias AdventOfCode.Helpers, as: HelperUtils
+
+  @outcomes %{
+    A: 1,
+    B: 2,
+    C: 3,
+    X: 1,
+    Y: 2,
+    Z: 3
+  }
+
+  defp get_score([3, 1]), do: 1 + 6
+  defp get_score([1, 3]), do: 3
+  defp get_score([them, us]) when them > us, do: us
+  defp get_score([them, us]) when them < us, do: us + 6
+  defp get_score([them, us]) when them == us, do: us + 3
+
+  defp get_outcome([1, 1]), do: 3
+  defp get_outcome([them, 1]), do: rem(them - 1, 3)
+  defp get_outcome([them, 2]), do: them
+  defp get_outcome([2, 3]), do: 3
+  defp get_outcome([them, 3]), do: rem(them + 1, 3)
+
+  def elf_moves(),
+    do:
+      __DIR__
+      |> Path.join("input.txt")
+      |> HelperUtils.file_input()
+      |> String.split([" ", "\n"], trim: true)
+      |> Enum.chunk_every(2)
+
+  def part_1 do
+    elf_moves()
+    |> Enum.map(fn [them, us] ->
+      [them, us]
+      |> Enum.map(fn key ->
+        @outcomes
+        |> Map.get(String.to_atom(key))
+      end)
+      |> get_score()
+    end)
+    |> Enum.sum()
+  end
+
+  def part_2 do
+    elf_moves()
+    |> Enum.map(fn [them, us] ->
+      our_move =
+        [them, us]
+        |> Enum.map(fn key ->
+          @outcomes
+          |> Map.get(String.to_atom(key))
+        end)
+        |> get_outcome()
+
+      [Map.get(@outcomes, String.to_atom(them)), our_move]
+      |> get_score()
+    end)
+    |> Enum.sum()
+  end
+
+  def answers do
+    %{part_1: part_1(), part_2: part_2()}
+  end
+end
