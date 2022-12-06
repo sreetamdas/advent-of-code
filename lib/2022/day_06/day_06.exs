78dcb6200d67de98af49093ea0c4de56470b9044
Sreetam Das <sreetamdas@gmail.com>
2022-12-06 11:06:28 +0530

    feat: day 06

---
+++ lib/2022/day_06/day_06.exs
@@ -0,0 +1,49 @@
+defmodule TuningTrouble do
+  @moduledoc """
+  Solution for Day 6 of Advent of Code 2022
+  """
+
+  @scan_size_part_1 4
+  @scan_size_part_2 14
+
+  defp get_cleaned_input(input),
+    do:
+      input
+      |> String.trim_trailing()
+      |> String.split("\n")
+      |> Enum.flat_map(fn x -> x |> String.codepoints() end)
+
+  defp scan_signal(signal, scan_size) do
+    signal
+    |> Enum.chunk_every(scan_size, 1)
+    |> Enum.reduce_while(scan_size, fn chunk, res ->
+      chunk
+      |> Enum.uniq()
+      |> then(fn scanned ->
+        case Enum.count(scanned) do
+          ^scan_size -> {:halt, res}
+          _ -> {:cont, res + 1}
+        end
+      end)
+    end)
+  end
+
+  def part_1(input),
+    do:
+      input
+      |> get_cleaned_input()
+      |> scan_signal(@scan_size_part_1)
+      |> IO.inspect(label: "part_1")
+
+  def part_2(input),
+    do:
+      input
+      |> get_cleaned_input()
+      |> scan_signal(@scan_size_part_2)
+      |> IO.inspect(label: "part_2")
+end
+
+input = File.read!("input.txt")
+
+TuningTrouble.part_1(input)
+TuningTrouble.part_2(input)
