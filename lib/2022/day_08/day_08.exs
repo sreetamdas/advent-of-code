7a076d7af90ea3acadf442f89fef80a2f7322ce0
Sreetam Das <sreetamdas@gmail.com>
2022-12-08 20:06:30 +0530

    improvement: cleanup 2022/day_08

--- lib/2022/day_08/day_08.exs
+++ lib/2022/day_08/day_08.exs
@@ -14,28 +14,30 @@ defmodule TreetopTreeHouse do
         |> Enum.map(&String.to_integer/1)
       end)
 
-  defp check_is_visible_from_edges(tree_row, index) do
-    cond do
-      index == 0 ->
-        true
+  # trees on the left and right of our current base tree
+  defp get_left_right_trees(trees, index),
+    do: {Enum.slice(trees, 0, index) |> Enum.reverse(), Enum.slice(trees, (index + 1)..-1//1)}
 
-      index == Enum.count(tree_row) - 1 ->
-        true
+  defp check_is_visible_from_edges(tree_row) do
+    tree_row
+    |> Enum.with_index(fn _, index ->
+      cond do
+        index == 0 || index == Enum.count(tree_row) - 1 ->
+          true
 
-      true ->
-        Enum.at(tree_row, index) >
-          Enum.max(Enum.slice(tree_row, 0, index)) ||
-          Enum.at(tree_row, index) >
-            Enum.max(Enum.slice(tree_row, (index + 1)..-1//1))
-    end
+        true ->
+          base_tree = Enum.at(tree_row, index)
+
+          {left, right} = get_left_right_trees(tree_row, index)
+
+          base_tree > Enum.max(left) || base_tree > Enum.max(right)
+      end
+    end)
   end
 
   defp get_line_of_sight([next_tree | trees_line_of_sight], h) do
     cond do
-      next_tree >= h ->
-        1
-
-      trees_line_of_sight == [] ->
+      next_tree >= h || trees_line_of_sight == [] ->
         1
 
       true ->
@@ -43,50 +45,32 @@ defmodule TreetopTreeHouse do
     end
   end
 
-  defp check_line_of_sight(tree_row, index) do
-    cond do
-      index == 0 ->
-        0
-
-      index == Enum.count(tree_row) - 1 ->
-        0
-
-      true ->
-        base_tree = Enum.at(tree_row, index)
-
-        {left, right} =
-          {Enum.slice(tree_row, 0, index) |> Enum.reverse(),
-           Enum.slice(tree_row, (index + 1)..-1//1)}
-
-        get_line_of_sight(left, base_tree) * get_line_of_sight(right, base_tree)
-    end
-  end
-
-  defp check_row_visibility(tree_row, func) do
+  defp check_line_of_sight(tree_row) do
     tree_row
-    |> Enum.with_index(fn tree, i ->
-      {tree, func.(tree_row, i)}
+    |> Enum.with_index(fn _, index ->
+      cond do
+        index == 0 || index == Enum.count(tree_row) - 1 ->
+          0
+
+        true ->
+          base_tree = Enum.at(tree_row, index)
+
+          {left, right} = get_left_right_trees(tree_row, index)
+
+          get_line_of_sight(left, base_tree) * get_line_of_sight(right, base_tree)
+      end
     end)
   end
 
   defp compute_trees(input, visiblity_func, score_fn) do
-    rotated_90 =
-      input
-      |> Enum.zip_with(& &1)
-
     visibility_horizontal =
       input
-      |> Enum.map(fn row ->
-        row
-        |> check_row_visibility(visiblity_func)
-      end)
+      |> Enum.map(&visiblity_func.(&1))
 
     visibility_vertical =
-      rotated_90
-      |> Enum.map(fn row ->
-        row
-        |> check_row_visibility(visiblity_func)
-      end)
+      input
+      |> Enum.zip_with(& &1)
+      |> Enum.map(&visiblity_func.(&1))
 
     visibility_vertical
     |> Enum.zip_with(& &1)
@@ -101,10 +85,7 @@ defmodule TreetopTreeHouse do
   def part_1(input) do
     input
     |> parse_input()
-    |> compute_trees(&check_is_visible_from_edges/2, fn {_, vis_h}, {_, vis_v} ->
-      vis_h ||
-        vis_v
-    end)
+    |> compute_trees(&check_is_visible_from_edges/1, &(&1 || &2))
     |> Enum.count(&(&1 == true))
     |> IO.inspect(label: "part_1")
   end
@@ -112,10 +93,7 @@ defmodule TreetopTreeHouse do
   def part_2(input) do
     input
     |> parse_input()
-    |> compute_trees(&check_line_of_sight/2, fn {_, vis_h}, {_, vis_v} ->
-      vis_h *
-        vis_v
-    end)
+    |> compute_trees(&check_line_of_sight/1, &(&1 * &2))
     |> Enum.max()
     |> IO.inspect(label: "part_2")
   end
@@ -124,5 +102,4 @@ end
 input = File.read!("input.txt")
 
 TreetopTreeHouse.part_1(input)
-
 TreetopTreeHouse.part_2(input)
