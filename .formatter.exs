bf1b95affffa111e4d7ca78aa32bf66786aac61b
Sreetam Das <sreetamdas@gmail.com>
2024-12-01 11:41:25 -0500

    update formatter for 2024

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test,2015,2022,2023}/**/*.{ex,exs}"]
+  inputs: [
+    "{mix,.formatter}.exs",
+    "{config,lib,test,2015,2022,2023,2024}/**/*.{ex,exs}"
+  ],
+  line_length: 60
 ]
