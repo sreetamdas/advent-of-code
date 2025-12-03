72c77bf9e03a350973aeb1caa2b6632a7fda61cf
Sreetam Das <sreetamdas@gmail.com>
2025-12-03 20:12:21 +0530

    upgrade deps, add 2025 to formatter

--- .formatter.exs
+++ .formatter.exs
@@ -2,7 +2,7 @@
 [
   inputs: [
     "{mix,.formatter}.exs",
-    "{config,lib,test,2015,2022,2023,2024}/**/*.{ex,exs}"
+    "{config,lib,test,2015,2022,2023,2024,2025}/**/*.{ex,exs}"
   ],
   line_length: 100
 ]
