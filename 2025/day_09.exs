a1c7093d4863d037813f4464c6002bffea24ad0d
Sreetam Das <sreetamdas@gmail.com>
2025-12-10 00:53:01 +0530

    optimise day_09, 21.8s -> 264.4ms

--- 2025/day_09.exs
+++ 2025/day_09.exs
@@ -10,13 +10,50 @@ defmodule MovieTheater do
     end)
   end
 
-  def area({first, second}), do: area(first, second)
+  def area({first, second} = input) when is_tuple(input), do: area(first, second)
+  def area([first, second] = input) when is_list(input), do: area(first, second)
   def area([x, y], [a, b]), do: area({x, y}, {a, b})
 
   def area({x, y}, {a, b}) do
     [[a, x], [b, y]]
     |> Enum.map(fn coord -> Enum.sort(coord, :desc) |> then(fn [i, j] -> i - j + 1 end) end)
     |> Enum.product()
+  end
+
+  def compress_coords(points) do
+    points
+    |> Enum.zip()
+    |> Enum.map(fn coord_list ->
+      coord_list
+      |> Tuple.to_list()
+      |> Enum.uniq()
+      |> Enum.sort()
+      |> Enum.with_index()
+      |> Map.new()
+    end)
+    |> then(fn [x_map, y_map] ->
+      Enum.map(points, fn [x, y] ->
+        [x_map[x], y_map[y]]
+      end)
+    end)
+  end
+
+  def decompress_coords(compressed_coords, original_coords) do
+    original_coords
+    |> Enum.zip()
+    |> Enum.map(fn coord_list ->
+      coord_list
+      |> Tuple.to_list()
+      |> Enum.uniq()
+      |> Enum.sort()
+      |> Enum.with_index()
+      |> Map.new(fn {coord, idx} -> {idx, coord} end)
+    end)
+    |> then(fn [x_reverse_map, y_reverse_map] ->
+      Enum.map(compressed_coords, fn [x_idx, y_idx] ->
+        [x_reverse_map[x_idx], y_reverse_map[y_idx]]
+      end)
+    end)
   end
 
   defp on_edge?(points, [px, py]) do
@@ -83,10 +120,14 @@ defmodule MovieTheater do
     input
     |> parse_input()
     |> then(fn parsed_reds ->
-      parsed_reds
+      compressed_coords =
+        parsed_reds
+        |> compress_coords()
+
+      compressed_coords
       |> Enum.with_index()
       |> Enum.flat_map(fn {red_pos, index} ->
-        parsed_reds
+        compressed_coords
         |> Enum.slice((index + 1)..-1//1)
         |> Enum.map(fn other_red ->
           [red_pos, other_red]
@@ -94,13 +135,13 @@ defmodule MovieTheater do
       end)
       |> Enum.map(fn [a, b] = tiles -> {tiles, area(a, b)} end)
       |> Enum.sort_by(fn {_, area} -> area end, :desc)
-      |> Enum.reduce_while(nil, fn {tiles = [[a, b] = first, [x, y] = second], area}, _ ->
+      |> Enum.reduce_while(nil, fn {tiles = [[a, b] = first, [x, y] = second], _area}, _ ->
         [third, fourth] = [[a, y], [x, b]]
 
         all_four = [first, third, second, fourth]
 
         cond do
-          Enum.all?(all_four, &in_polygon(parsed_reds, &1)) ->
+          Enum.all?(all_four, &in_polygon(compressed_coords, &1)) ->
             all_four
             |> Enum.chunk_every(2, 1, :discard)
             |> Enum.all?(fn [_p1 = [x1, y1], _p2 = [x2, y2]] ->
@@ -123,10 +164,10 @@ defmodule MovieTheater do
                     end)
                 end
 
-              Enum.all?(all_four_edges, &in_polygon(parsed_reds, &1))
+              Enum.all?(all_four_edges, &in_polygon(compressed_coords, &1))
             end)
             |> case do
-              true -> {:halt, area}
+              true -> {:halt, tiles}
               false -> {:cont, tiles}
             end
 
@@ -134,6 +175,8 @@ defmodule MovieTheater do
             {:cont, tiles}
         end
       end)
+      |> decompress_coords(parsed_reds)
+      |> area()
     end)
     |> tap(fn _ -> :ets.delete(:aoc_2025_day_09) end)
   end
@@ -141,15 +184,15 @@ end
 
 # ##### With input prod #####
 # Name             ips        average  deviation         median         99th %
-# part_1         62.08       0.0161 s     ±3.13%       0.0161 s       0.0189 s
-# part_2        0.0460        21.76 s     ±3.03%        21.76 s        22.23 s
+# part_1         58.71       17.03 ms    ±13.40%       16.25 ms       24.48 ms
+# part_2          3.73      268.09 ms     ±5.44%      264.43 ms      311.90 ms
 
 # Comparison:
-# part_1         62.08
-# part_2        0.0460 - 1351.02x slower +21.75 s
+# part_1         58.71
+# part_2          3.73 - 15.74x slower +251.06 ms
 
 # Memory usage statistics:
 
 # Name      Memory usage
 # part_1       0.0590 GB
-# part_2       141.59 GB - 2400.80x memory usage +141.53 GB
+# part_2         1.70 GB - 28.87x memory usage +1.64 GB
